0b78ceff6bbd2425c341e2c124540526359506c2
xDareDevil2003x <cricafogo2003@gmail.com>
2024-10-07 21:35:50 -0500

    Update config.py

--- PythonScript/src/config.py
+++ PythonScript/src/config.py
@@ -1,11 +1,11 @@
 # CoinGecko API (cuenta DEMO)
 COINGECKO_URL = 'https://api.coingecko.com/api/v3'
-COINGECKO_KEYLABEL = 'CcfgKonfioTest'
-COINGECKO_KEY = 'CG-Vm4voKYqsLxMF56owNrM4oVv'
+COINGECKO_KEYLABEL = '<key name>'
+COINGECKO_KEY = '<key>'
 
 # configuracion DataBase
 DB_NAME = 'bitcoins'
 TABLE_NAME = 'DataHistoricalCoins'
 
 # Ruta del export de la data
-PATH_EXPORT_EXCEL = 'C:/Users/criforer/Downloads/varios/Konfio/PythonScript/TestKonfioCCFGData.xlsx'
+PATH_EXPORT_EXCEL = '<path donde se va a almacenar el extracto de la data C://>'
